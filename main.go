e0cb0101e494151c332e39b3ae4dfcbaba8ea067
Timur Danilchenko <timur.danilchenko01@gmail.com>
2025-08-12 22:59:34 +0300

    solution version 1

--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+type Task func()
+
+func WorkerPool(m []Task, n int) {
+	tasksChan := make(chan Task, len(m))
+	waitWorkers := make(chan struct{}, n)
+
+	for range n {
+		go func() {
+			defer func() {
+				waitWorkers <- struct{}{}
+			}()
+			for true {
+				task, ok := <-tasksChan
+				if !ok {
+					break
+				}
+				task()
+			}
+		}()
+	}
+
+	for _, task := range m {
+		tasksChan <- task
+	}
+	close(tasksChan)
+
+	for range n {
+		<-waitWorkers
+	}
+}
